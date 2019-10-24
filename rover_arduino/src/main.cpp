35bffdb843e6ea247e7882f61bee9630c659ac2c
vililichi <keven0230@live.ca>
2019-10-24 14:14:23 -0400

    ajout servo-moteurs du bras

--- rover_arduino/src/main.cpp
+++ rover_arduino/src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <ros.h>
 #include "rover_control/CamCommand.h"
 #include "ServoMoteur.h"
@@ -8,6 +7,13 @@
   et le servo Horizontal la pin 12 */
 ServoMoteur servoCamVertical(13,20000,1500,2500,90,-90); 
 ServoMoteur servoCamHorizontal(12,20000,1500,2500,90,-90);
+//servo-moteurs pour bras untilisation des pin 2 à 7
+ServoMoteur servoPince(2); 
+ServoMoteur servoPoignet(3); 
+ServoMoteur servoJ2(4); 
+ServoMoteur servoJ1(5); 
+ServoMoteur servoJ0(6);
+ServoMoteur servoBase(7); 
 
 //node
 ros::NodeHandle nh;
@@ -29,12 +35,22 @@ void setup() {
   //initialisation de la node et du subcriber
   nh.initNode();
   nh.subscribe(ear);
+
+  servoJ2.setAngle(90);
+  servoJ1.setAngle(45);
+  servoJ0.setAngle(0);
 }
 
 void loop() {
   //mise à jour des servo-moteurs
   servoCamVertical.update();
   servoCamHorizontal.update();
+  servoPince.update();
+  servoPoignet.update();
+  servoJ2.update();
+  servoJ1.update();
+  servoJ0.update();
+  servoBase.update();
   //mise à jour de la node
   nh.spinOnce();
 }
