97d07acdcdb8478f114f5005794bfe91026ba329
Philippe Warren <philippewarren31@gmail.com>
2020-02-10 14:49:18 -0500

    Added the code that takes care of taking panoramas

--- rover_arduino/src/main.cpp
+++ rover_arduino/src/main.cpp
@@ -11,7 +11,7 @@ Servo servoCamVertical;
 Servo servoCamHorizontal;
 Encodeur controlServoH('A0');
 int objectifServoHorizontal = 0;
-
+bool panoEnCours = false;
 
 //node
 ros::NodeHandle nh;
@@ -19,45 +19,83 @@ ros::NodeHandle nh;
 // callback ros
 void servoCam_cb (const rover_udes::CamCommand &angles)
 {
-  objectifServoHorizontal = angles.cam_vertical;
-  servoCamVertical.writeMicroseconds((int)1500+angles.cam_horizontal*500/90);
+	if (angles.is_pano and !panoEnCours)
+	{
+		panoEnCours = true;
+	}
+
+	if (panoEnCours)
+	{
+		objectifServoHorizontal = 0;
+		servoCamVertical.writeMicroseconds((int)1500);
+	}
+	else
+	{
+		objectifServoHorizontal = angles.cam_horizontal;
+		servoCamVertical.writeMicroseconds((int)1500+angles.cam_vertical*500/90);
+	}
+
+
+	
 }
 
 //ajustement servoHorizontal
 void updateServoCamH()
 {
-  const float tourEncParTourServo = 4;
-  float position = controlServoH.ReadAngle() / tourEncParTourServo;
-  int delta = objectifServoHorizontal - position;
-  while (delta >= 180)delta -= 360;
-  while (delta < -180)delta += 360;
-  int vitesse = 1500;
-  if (delta > 5) vitesse = 1700;
-  else if (delta > 0) vitesse = 1600;
-  else if (delta < -5) vitesse = 1300;
-  else if (delta < 0) vitesse = 1400;
+	const float tourEncParTourServo = 4;
+	float position = controlServoH.ReadAngle() / tourEncParTourServo;
+	static bool debutPano = true;
 
-  servoCamHorizontal.writeMicroseconds(vitesse);
+	if (panoEnCours)
+	{
+		float posInit;
+		if (debutPano)
+		{
+			posInit = position;
+			debutPano = false;
+		}
+
+		if (position < (posInit + 360))
+		{
+			servoCamHorizontal.writeMicroseconds(1600);
+		}
+		else
+		{
+			panoEnCours = false;
+			debutPano = true;
+		}
+	}
+	else
+	{	
+		int delta = objectifServoHorizontal - position;
+		while (delta >= 180)delta -= 360;
+		while (delta < -180)delta += 360;
+		int vitesse = 1500;
+		if (delta > 5) vitesse = 1700;
+		else if (delta > 0) vitesse = 1600;
+		else if (delta < -5) vitesse = 1300;
+		else if (delta < 0) vitesse = 1400;
+
+		servoCamHorizontal.writeMicroseconds(vitesse);
+	}
 }
-
 
 //subcriber utilisant le callback servo_cb
 ros::Subscriber<rover_udes::CamCommand> ear ("cam_cmd",servoCam_cb);
 
+void setup()
+{
+	servoCamHorizontal.attach(9);
+	servoCamVertical.attach(10);
+	gestionaireTimer2::ajouter(updateServoCamH);
 
-
-void setup() {
-
-  servoCamHorizontal.attach(9);
-  servoCamVertical.attach(10);
-  gestionaireTimer2::ajouter(updateServoCamH);
-
-  Serial.begin(57600);
-  nh.initNode();
-  nh.subscribe(ear);
+	Serial.begin(57600);
+	nh.initNode();
+	nh.subscribe(ear);
 }
 
-void loop() {
-  //mise à jour de la node
-  nh.spinOnce();
+void loop()
+{
+	//mise à jour de la node
+	nh.spinOnce();
 }
