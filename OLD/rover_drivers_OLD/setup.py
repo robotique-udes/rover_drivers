e7b5dece77b8a9daafa61f765e6523d4aff02b86
philippewarren <philippewarren31@gmail.com>
2022-10-04 19:39:09 -0400

    Cleanup and move old or untested stuff to OLD

--- OLD/rover_drivers_OLD/setup.py
+++ OLD/rover_drivers_OLD/setup.py
@@ -3,6 +3,6 @@
 from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-setup_args = generate_distutils_setup(packages=["rover_drivers"], package_dir={"": "src"})
+setup_args = generate_distutils_setup(packages=["rover_drivers_OLD"], package_dir={"": "src"})
 
 setup(**setup_args)
